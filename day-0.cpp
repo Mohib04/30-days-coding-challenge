da65830b443c5f933542b823f2f0638b08a7c091
Mohibbull Mohib <33421746+Mohib04@users.noreply.github.com>
2021-07-04 16:16:51 +0600

    day one done

---
+++ day-0.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string inputString;
+    ///to get full paragraph
+    getline(cin, inputString);
+    cout<<"Hello, World."<<endl<<inputString<<endl;
+}
