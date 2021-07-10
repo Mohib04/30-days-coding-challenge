a758b7f0a07603efba184c9616f949d0146bde68
Mohibbull Mohib <33421746+Mohib04@users.noreply.github.com>
2021-07-10 09:23:19 +0600

    done

---
+++ day-5/day--5.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+
+    int n;
+    ///Enter the any integer number
+    cin>>n;
+    for(int i=1; i<=10; i++){
+        cout<<n<<" x "<<i<<" = "<<n*i<<endl;
+    }
+
+    return 0;
+}
